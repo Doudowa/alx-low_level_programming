7e5886e60f4c633d41fead13b1e1ff9f5e174a69
doudowa <arifidouaa6@gmail.com>
2023-12-11 13:23:27 -0800

    task1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -fPIC -c *.c
-gcc -shared *.O -o liball.so
+gcc -c -fpic .c
+gcc -shared -o liball.so.o
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -2,15 +2,27 @@
 #define MAIN_H
 
 #include <stdlib.h>
+#include <stdio.h>
 
 int _putchar(char c);
-void _puts(char *str);
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int _atoi(const char *s);
-void print_int(unsigned long int n);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char s);
+void _puts(chars);
+char _strcpy(chardest, char src);
+int _atoi(chars);
+char _strcat(chardest, char src);
+char_strncat(char dest, charsrc, int n);
+char _strncpy(chardest, char src, int n);
+int _strcmp(chars1, char s2);
+char_memset(char s, char b, unsigned int n);
+char_memcpy(char dest, charsrc, unsigned int n);
+char _strchr(chars, char c);
+unsigned int _strspn(char s, characcept);
+char _strpbrk(chars, char accept);
+char_strstr(char haystack, charneedle);
 
 #endif
